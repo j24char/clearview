9899dd91baead315fd4f0254eec6ee7eda702c5a
j24char <justinacharron@gmail.com>
2026-03-30 12:43:18 -0500

    Updated colors in theme.ts

--- constants/theme.ts
+++ constants/theme.ts
@@ -1,6 +1,6 @@
 import { Platform } from 'react-native';
 
-const tintColorLight = '#1F6F63';
+const tintColorLight = '#00355e';
 const tintColorDark = '#EAF6F3';
 
 export const Colors = {
@@ -8,16 +8,16 @@ export const Colors = {
     text: '#11221E',
     background: '#ffffff',
     tint: tintColorLight,
-    icon: '#64827C',
-    tabIconDefault: '#64827C',
+    icon: '#647f82',
+    tabIconDefault: '#647e82',
     tabIconSelected: tintColorLight,
   },
   dark: {
     text: '#EAF6F3',
     background: '#12211E',
     tint: tintColorDark,
-    icon: '#A3C7C0',
-    tabIconDefault: '#A3C7C0',
+    icon: '#a3c1c7',
+    tabIconDefault: '#a3c1c7',
     tabIconSelected: tintColorDark,
   },
 };
@@ -25,13 +25,13 @@ export const Colors = {
 export const AppColors = {
   background: '#ffffff',
   card: '#FFFFFF',
-  cardAlt: '#F8FBFA',
-  hero: '#D8EEE7',
-  ink: '#16302A',
-  subtleText: '#4E6A63',
-  accent: '#6FB7A7',
-  accentSoft: '#E2F5EF',
-  accentDeep: '#1F6F63',
+  cardAlt: '#f8fbfb',
+  hero: '#d8ebee',
+  ink: '#015190', //'#16302A',
+  subtleText: '#4e676a',
+  accent: '#cdeefd', //'#6FB7A7',
+  accentSoft: '#e2f3f5',
+  accentDeep: '#00355e', //'#1F6F63',
   line: '#C8DDD7',
 };
 
